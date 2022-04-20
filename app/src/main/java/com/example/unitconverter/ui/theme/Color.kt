6848a3b98e663c45c25922227160261e452eb9d3
6210612617 <6210612617@student,tu.ac.th>
2022-04-21 01:49:23 +0700

    commit

---
+++ app/src/main/java/com/example/unitconverter/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.example.unitconverter.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val green = Color(0xFFb2fab4)
+val green100 = Color(0xFF00897b)
+val green200 = Color(0xFF4ebaaa)
+val green500 = Color(0xFF81c784)
+
---
+++ app/src/main/java/com/example/unitconverter/ui/theme/Theme.kt
@@ -0,0 +1,47 @@
+package com.example.unitconverter.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = green500,
+    primaryVariant = green100,
+    secondary = green500
+)
+
+private val LightColorPalette = lightColors(
+    primary = green,
+    primaryVariant = green200,
+    secondary = green
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun UnitConverterTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
